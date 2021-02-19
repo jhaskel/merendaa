63f89f8aaf6d2dacb8d532141724ac8a98ff5e54
Joao Haskel <johaskel@gmail.com>
2021-02-18 21:16:33 -0300

    normalização

--- src/main/java/com/doisbitsw/licencas/api/compras/ComprasRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/compras/ComprasRepository.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 public interface ComprasRepository extends JpaRepository<Compras, Long> {
 
-    @Query(value = "SELECT *  FROM pedido_itens WHERE  af = 0;", nativeQuery = true)
+    @Query(value = "SELECT *  FROM itens WHERE  af = 0;", nativeQuery = true)
     List<Compras> findAll();
 
-    @Query(value = "SELECT * FROM pedido_itens WHERE af > 0  GROUP BY af;", nativeQuery = true)
+    @Query(value = "SELECT * FROM itens WHERE af > 0  GROUP BY af;", nativeQuery = true)
     List<Compras> findAll2();
 
-    @Query(value = "SELECT * FROM pedido_itens GROUP BY pedido;", nativeQuery = true)
+    @Query(value = "SELECT * FROM itens GROUP BY pedido;", nativeQuery = true)
     List<Compras> findAll3();
 
 
-    @Query(value = "SELECT * FROM pedido_itens WHERE pedido = :pedido and ischeck = false;", nativeQuery = true)
+    @Query(value = "SELECT * FROM itens WHERE pedido = :pedido and ischeck = false;", nativeQuery = true)
     List<Compras> findByPedido(String pedido);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod AS nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod AS nomec FROM itens ite\n" +
             "\n" +
             " WHERE ite.pedido = '1611942997' \n" +
             " AND ite.ativo = true  \n" +
@@ -29,87 +29,87 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
             " ORDER BY ite.fornecedor, ite.alias  ", nativeQuery = true)
     List<Compras> findByPedidoAll(String pedido);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod AS nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod AS nomec FROM itens ite\n" +
             " WHERE ite.af = :af\n" +
             " AND ite.ativo = true  \n" +
             " GROUP BY ite.id\n" +
             " ORDER BY ite.fornecedor, ite.alias ", nativeQuery = true)
     List<Compras> findByAf(Long af);
 
-    @Query(value = "SELECT * FROM pedido_itens  WHERE escola = :escola  AND pedido = :pedido ", nativeQuery = true)
+    @Query(value = "SELECT * FROM itens  WHERE escola = :escola  AND pedido = :pedido ", nativeQuery = true)
     List<Compras> findByEscola(Long escola, Long pedido);
 
-    @Query(value = "SELECT * FROM pedido_itens ite\n" +
+    @Query(value = "SELECT * FROM itens ite\n" +
             "INNER join af ON af.code = ite.af\n" +
             " WHERE af.ativo = TRUE and ite.escola = :escola  AND ite.ano = :ano ", nativeQuery = true)
     List<Compras> findEscolar(Long escola, Long ano);
 
-    @Query(value = "SELECT ite.* FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.* FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             " WHERE ite.fornecedor = :fornecedor and af > 0 AND af.ativo = TRUE \n" +
             " ", nativeQuery = true)
     List<Compras> findByFornecedor(Long fornecedor);
 
-    @Query(value = "SELECT * FROM pedido_itens ite \n" +
+    @Query(value = "SELECT * FROM itens ite \n" +
             "                   INNER JOIN af ON af.code = ite.af          \n" +
             "                     WHERE af.ativo= true and ite.ano = :ano AND ite.af > 0 ", nativeQuery = true)
     List<Compras> findEscolaAll(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.quantidade) as tot,ite.cod AS nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.quantidade) as tot,ite.cod AS nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE ite.ano = :ano and af > 0 AND af.ativo = TRUE\n" +
             "GROUP BY ite.produto\n" +
             "ORDER BY tot DESC ,ite.categoria,ite.alias", nativeQuery = true)
     List<Compras> findItensAno(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano\n" +
             "GROUP BY ite.mes ", nativeQuery = true)
     List<Compras> findTotalMes(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano and ite.nivel = :nivel\n" +
             "GROUP BY ite.mes ", nativeQuery = true)
     List<Compras> findTotalMesNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot,ite.cod as nomec  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano and ite.escola = :escola\n" +
             "GROUP BY ite.mes ", nativeQuery = true)
     List<Compras> findTotalMesEscola(Long escola, Long ano);
 
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "INNER JOIN categoria cat ON cat.id = ite.categoria\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano and ite.nivel = :nivel\n" +
             "GROUP BY ite.categoria ", nativeQuery = true)
     List<Compras> findTotalCategoriaNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "INNER JOIN categoria cat ON cat.id = ite.categoria\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano and ite.escola = :escola\n" +
             "GROUP BY ite.categoria ", nativeQuery = true)
     List<Compras> findTotalCategoriaEscola(Long escola, Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, cat.nome as nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "INNER JOIN categoria cat ON cat.id = ite.categoria\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano\n" +
             "GROUP BY ite.categoria ", nativeQuery = true)
     List<Compras> findTotalCategoria(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, esc.alias as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, esc.alias as nomec FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            INNER JOIN unidade_escolar esc ON esc.id = ite.escola \n" +
             "            WHERE af.ativo = TRUE AND ite.ano = :ano \n" +
             "            GROUP BY ite.escola", nativeQuery = true)
     List<Compras> findTotalEscolas(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, esc.alias as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total) AS tot, esc.alias as nomec FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            INNER JOIN unidade_escolar esc ON esc.id = ite.escola \n" +
             "            WHERE af.ativo = TRUE AND ite.ano = :ano and ite.nivel = :nivel\n" +
@@ -117,14 +117,14 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
     List<Compras> findTotalEscolaNivel(Long nivel, Long ano);
 
 
-    @Query(value = "SELECT ite.*,sum(ite.total/esc.alunos) AS tot, esc.alias as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total/esc.alunos) AS tot, esc.alias as nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "INNER JOIN unidade_escolar esc ON esc.id = ite.escola\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano AND esc.alunos > 0\n" +
             "GROUP BY ite.escola ", nativeQuery = true)
     List<Compras> findMediaAlunos(Long ano);
 
-    @Query(value = "SELECT ite.*,sum(ite.total/esc.alunos) AS tot, esc.alias as nomec FROM pedido_itens ite\n" +
+    @Query(value = "SELECT ite.*,sum(ite.total/esc.alunos) AS tot, esc.alias as nomec FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "INNER JOIN unidade_escolar esc ON esc.id = ite.escola\n" +
             "WHERE af.ativo = TRUE AND ite.ano = :ano AND esc.alunos > 0 and ite.nivel = :nivel\n" +
@@ -132,18 +132,18 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
     List<Compras> findMediaAlunosNivel(Long nivel, Long ano);
 
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 ", nativeQuery = true)
     double findTotal(Long ano);
 
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 AND ite.nivel = :nivel ", nativeQuery = true)
     double findTotalNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 AND ite.escola = :escola ", nativeQuery = true)
     double findTotalEscola(Long escola, Long ano);
@@ -151,37 +151,37 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
 
 
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.escola = :escola AND ite.af > 0 ", nativeQuery = true)
     double findSoma(Long escola);
 
-    @Query(value = "SELECT sum(ite.total) as totalAgro  FROM pedido_itens ite \n" +
+    @Query(value = "SELECT sum(ite.total) as totalAgro  FROM itens ite \n" +
             "INNER JOIN af ON af.code = ite.af          \n" +
             "WHERE af.ativo= true  and ano = :ano and ite.escola = :escola AND ite.af > 0 AND ite.isagro = true", nativeQuery = true)
     double findTotalAgroEscola(Long escola, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true and ite.nivel=:nivel AND ite.isagro = true and ite.ano = :ano AND ite.af > 0 ", nativeQuery = true)
     double findTotalAgroNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true  AND ite.isagro = true and ite.ano = :ano AND ite.af > 0 ", nativeQuery = true)
     double findTotalAgro(Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as totalPedido  FROM pedido_itens ite \n" +
+    @Query(value = "SELECT sum(ite.total) as totalPedido  FROM itens ite \n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.pedido = :pedido AND ite.af > 0 ", nativeQuery = true)
     double findTotalPedido(String pedido);
 
-    @Query(value = "SELECT sum(ite.total) as totalPedido  FROM pedido_itens ite \n" +
+    @Query(value = "SELECT sum(ite.total) as totalPedido  FROM itens ite \n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true  and ite.af = :af AND ite.af > 0 ", nativeQuery = true)
     double findTotalAf(Long af);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 \n" +
             "\t\t\t\tAND (ite.categoria = 1 OR ite.categoria = 2 \n" +
@@ -190,7 +190,7 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
             "OR ite.categoria = 6) AND ite.isagro = FALSE ", nativeQuery = true)
     double findTradicional(Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 \n" +
             "\t\t\t\tAND (ite.categoria = 1 OR ite.categoria = 2 \n" +
@@ -199,7 +199,7 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
             "OR ite.categoria = 6) AND ite.isagro = FALSE and ite.nivel = :nivel", nativeQuery = true)
     double findTradicionalNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "            INNER JOIN af ON af.code = ite.af\n" +
             "            WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0 \n" +
             "\t\t\t\tAND (ite.categoria = 1 OR ite.categoria = 2 \n" +
@@ -208,19 +208,19 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
             "OR ite.categoria = 6) AND ite.isagro = FALSE and ite.escola = :escola", nativeQuery = true)
     double findTradicionalEscola(Long escola, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.af > 0\n" +
             "AND (ite.categoria = 4 OR ite.categoria = 7)  ", nativeQuery = true)
     double findDiversos(Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.nivel = :nivel AND ite.af > 0\n" +
             "AND (ite.categoria = 4 OR ite.categoria = 7)  ", nativeQuery = true)
     double findDiversosNivel(Long nivel, Long ano);
 
-    @Query(value = "SELECT sum(ite.total) as tot  FROM pedido_itens ite\n" +
+    @Query(value = "SELECT sum(ite.total) as tot  FROM itens ite\n" +
             "INNER JOIN af ON af.code = ite.af\n" +
             "WHERE af.ativo= true   and ite.ano = :ano AND ite.escola = :escola AND ite.af > 0\n" +
             "AND (ite.categoria = 4 OR ite.categoria = 7)  ", nativeQuery = true)
--- src/main/java/com/doisbitsw/licencas/api/produtos/ProdutoRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/produtos/ProdutoRepository.java
@@ -14,7 +14,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     @Query(value = "SELECT * FROM produto p\n" +
             "            WHERE p.ativo = true and p.id NOT IN (\n" +
-            "\t\t\t\tSELECT produto FROM pedido_itens ite \n" +
+            "\t\t\t\tSELECT produto FROM itens ite \n" +
             "\t\t\t\tINNER JOIN pedido ped ON ped.code = ite.pedido\n" +
             "\t\t\t\tWHERE ite.escola = :escola AND ped.iscart = true\n" +
             "\t\t\t\t)\n" +
@@ -23,7 +23,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 
     @Query(value = "SELECT * FROM produto p\n" +
-            "WHERE p.ativo = true and p.id NOT IN (SELECT produto FROM pedido_itens ) ORDER BY p.categoria,p.nome", nativeQuery = true)
+            "WHERE p.ativo = true and p.id NOT IN (SELECT produto FROM itens ) ORDER BY p.categoria,p.nome", nativeQuery = true)
     List<Produto> findMenos();
 
     @Query(value = "select * from produto  where ativo = TRUE order by id desc ", nativeQuery = true)
