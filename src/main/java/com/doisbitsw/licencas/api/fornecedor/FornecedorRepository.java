975d177830cd113b30c83a33d472d5eabf2e3a23
Joao Haskel <johaskel@gmail.com>
2021-03-12 15:35:18 -0300

    contabilidade - orcamento

--- src/main/java/com/doisbitsw/licencas/api/fornecedor/FornecedorRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/fornecedor/FornecedorRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
+    @Query(value = "SELECT *  FROM fornecedor order by ativo DESC,nome", nativeQuery = true)
+    List<Fornecedor> findAll();
+
     @Query(value = "SELECT *  FROM fornecedor WHERE id = :id ", nativeQuery = true)
     List<Fornecedor> findId(Long id);
 
