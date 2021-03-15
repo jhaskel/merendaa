e99387706f910aa1f36486e87b4ad75b63135fc2
Joao Haskel <johaskel@gmail.com>
2021-03-15 10:29:06 -0300

    itens - mais

--- src/main/java/com/doisbitsw/licencas/api/compras/ComprasRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/compras/ComprasRepository.java
@@ -226,7 +226,10 @@ public interface ComprasRepository extends JpaRepository<Compras, Long> {
             "AND (ite.categoria = 4 OR ite.categoria = 7)  ", nativeQuery = true)
     double findDiversosEscola(Long escola, Long ano);
 
-    @Query(value = "SELECT ite.* FROM itens ite where ite.ano = :ano ", nativeQuery = true)
+    @Query(value = "SELECT *,SUM(ite.total) AS tot\n" +
+            "FROM itens ite\n" +
+            "where ite.ano = :ano\n" +
+            "group BY ite.produto ORDER BY tot desc", nativeQuery = true)
     List<Compras> findMais(Long ano);
 
 }
