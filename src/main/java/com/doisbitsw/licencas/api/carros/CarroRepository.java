6dd6afd98766c34b8428ed01979a4e02b4fa3d87
Joao Haskel <johaskel@gmail.com>
2021-04-23 16:57:38 -0300

    carro - novo

--- src/main/java/com/doisbitsw/licencas/api/carros/CarroRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/carros/CarroRepository.java
@@ -1,10 +1,12 @@
 package com.doisbitsw.licencas.api.carros;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CarroRepository extends JpaRepository<Carro, Long> {
-
+    @Query(value = " SELECT car.*,user.nome as teste FROM carro car\n" +
+            "INNER JOIN user  ON user.id = car.tipo", nativeQuery = true)
     List<Carro> findByTipo(String tipo);
 }
