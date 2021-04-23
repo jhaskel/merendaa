f90b2b78310de4f6b46f973d76df40870f549651
Joao Haskel <johaskel@gmail.com>
2021-04-23 16:21:32 -0300

    categoria - getAtivo

--- src/main/java/com/doisbitsw/licencas/api/usuario/UsuarioDTO.java
+++ src/main/java/com/doisbitsw/licencas/api/usuario/UsuarioDTO.java
@@ -9,7 +9,7 @@ public class UsuarioDTO {
     private String nome;
     private String email;
     private String login;
-    private String senha;
+
     private Long escola;
     private String nivel;
     private String recuperar;
